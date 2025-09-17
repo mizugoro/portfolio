daebbb6ea42a02f788205b5b9eab88bb187bb7c9
水野　悟朗（Mizuno　Goro) <57975358+mizugoro@users.noreply.github.com>
2025-09-17 09:17:29 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    // 出力先を 'docs' に変更
+    // デフォルトは 'dist'
+    outDir: 'docs',
+  },
 })
